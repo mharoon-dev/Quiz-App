7c19d62a0fe49eac0385485ba9f17d524e3eb1a6
mharoon-dev <haroon.aghani@gmail.com>
2023-12-24 17:32:43 +0500

    UI designing

---
+++ app.js
@@ -0,0 +1,9 @@
+let questions = [
+    {
+        question:"HTML stands for:",
+        option1: "Hyper Text Markup Language",
+        option2: "High Text Markup Language",
+        option2: "Hyper Text main Language"
+        
+    }
+]
